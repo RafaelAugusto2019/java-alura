ae71be91c5a61e8d86b92e8861426fa964ee5064
RafaelAugusto <rafaelauusto2019@gmail.com>
2026-05-10 23:28:00 -0300

    feat: new handler exceprion for bean validation

--- api/src/main/java/com/estudos/alura/api/exception/HandlerException.java
+++ api/src/main/java/com/estudos/alura/api/exception/HandlerException.java
@@ -2,6 +2,8 @@ package com.estudos.alura.api.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,22 @@ public class HandlerException {
         return ResponseEntity.notFound().build();
     }
 
-    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity exceptionMethodArgumentNotValid(MethodArgumentNotValidException exception){
+        var listErrors = exception.getFieldErrors();
+        
+        return ResponseEntity.badRequest().body(
+                listErrors.stream().map(ErrorResponse::new).toList());
+    }
+
+
+    private record ErrorResponse(String error, String detail){
+
+        public ErrorResponse(FieldError filFieldError){
+            this(filFieldError.getField(), filFieldError.getDefaultMessage());
+        }
+    }
+
 }
+
+
